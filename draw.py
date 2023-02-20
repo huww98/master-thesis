7638e8ed2df863b239b95024f993f9ff108e8af4
胡玮文 <sehuww@mail.scut.edu.cn>
2023-02-21 00:04:05 +0800

    draw

--- draw.py
+++ draw.py
@@ -28,14 +28,17 @@ def no_aa():
     X = X[None, :]
     Y = Y[:, None]
 
-    target = np.where((X + 0 * Y < 3.5)[..., None], [1,1,1], [0.5,0.5,0.6])
+    FG = np.array([1, 1, 1])
+    BG = np.array([0.6, 0.6, 0.7])
+
+    target = np.where((X + 0 * Y < 3.5)[..., None], FG, BG)
 
     fig, (axes_target, axes_model, axes_loss, axes_grad) = \
         plt.subplots(4, 3, figsize=(6.3, 4), sharex=True, sharey=False)
     fig.set_layout_engine('compressed')
 
     for ax in axes_target:
-        ax.imshow(target, interpolation='none', origin='lower', cmap='gray')
+        ax.imshow(target, interpolation='none', origin='lower')
         ax.grid(True, which='minor')
     for axes in [axes_target, axes_model]:
         for ax in axes:
@@ -47,6 +50,9 @@ def no_aa():
     model = np.where(2 * X - Y < 5.1, 1, 0)[..., None]
     axes_model[0].imshow(model, interpolation='none', origin='lower', cmap='gray')
 
+    n_sample = 256
+    sample = (np.arange(n_sample) + 0.5) / n_sample - 0.5
+
     for ax in axes_model:
         ax.grid(True, which='minor')
         ax.add_line(plt.Line2D([2.3, 3.8], [-0.5, 2.5], color='red', linewidth=1))
@@ -56,14 +62,34 @@ def no_aa():
                     color='red')
     axes_model[0].set_ylabel('渲染结果')
 
-    X_off = np.linspace(-0.5, 7.5, 1000)
-    model = np.where(2 * (X - X_off[:, None, None]) - Y < 0.5, 1, 0)[..., None]
-
-    loss = np.sum(np.abs(model - target), axis=(1, 2, 3))
+    X_off = np.linspace(-0.5, 7.5, 256)
+    model_batch = np.where(2 * (X - X_off[:, None, None]) - Y < 0.5, 1, 0)[..., None]
+    loss = np.sum(np.abs(model_batch - target), axis=(1, 2, 3))
     axes_loss[0].step(X_off, loss, color='red')
+    axes_grad[0].plot([-0.5, 7.5], [0, 0], color='red')
+
+    Xo = X_off[:, None, None, None, None]
+    def plot_to(idx):
+        axes_model[idx].imshow(model, interpolation='none', origin='lower', cmap='gray')
+        loss = np.sum(np.abs(model_batch - target), axis=(1, 2, 3))
+        axes_loss[idx].plot(X_off, loss, color='red')
+        grad = np.gradient(loss, X_off)
+        axes_grad[idx].axhline(0, color=[0.7]*3, linewidth=1)
+        axes_grad[idx].plot(X_off, grad, color='red')
+
+    model = 2 * (X[..., None, None] + sample) - (Y[..., None, None] + sample[:, None]) < 5.1
+    model = np.mean(model, axis=(-1, -2))[..., None]
+    model_batch = 2 * ((X[..., None, None] - Xo) + sample) - (Y[..., None, None] + sample[:, None]) < 0.5
+    model_batch = np.mean(model_batch, axis=(-1, -2))[..., None]
+    plot_to(1)
+
+    model = FG * model + BG * (1 - model)
+    model_batch = FG * model_batch + BG * (1 - model_batch)
+    plot_to(2)
+
+
     axes_loss[0].set_ylabel('损失函数')
 
-    axes_grad[0].plot([-0.5, 7.5], [0, 0], color='red')
     axes_grad[0].yaxis.set_major_locator(ticker.MultipleLocator(1))
     axes_grad[0].set_ylabel('梯度')
     axes_grad[0].set_xlabel('无可见性梯度')
